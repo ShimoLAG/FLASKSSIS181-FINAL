bb2fdc80da75ad0814e252437aa8773fd9c027bf
ShimoLAG <161853425+ShimoLAG@users.noreply.github.com>
2025-08-14 11:49:58 +0800

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import os
 
 mysql = MySQL()
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thisisarandomsecretkey1111' #encrypts the cookies and session data related to website (dont worry bout it)
-    
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
    
-    app.config['MYSQL_HOST'] = "localhost"
-    app.config['MYSQL_USER'] = "root"
-    app.config['MYSQL_PASSWORD'] = ""
-    app.config['MYSQL_DB'] = "fssis"
+    app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+    app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+    app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
     mysql.init_app(app)
 
